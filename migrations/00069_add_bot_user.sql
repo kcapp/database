30fa82593cadce0cd841757a1662258433e8a50f
Thord Setsaas <t.setsaas@sportradar.com>
2024-03-08 13:32:46 +0100

    Correctly add bot user

--- migrations/00069_add_bot_user.sql
+++ migrations/00069_add_bot_user.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 INSERT INTO player (first_name, last_name, vocal_name, nickname, slack_handle, color, profile_pic_url, smartcard_uid, board_stream_url, board_stream_css, active, office_id, is_bot, is_placeholder, created_at)
 SELECT 'kcapp-bot', '', 'kcappbot.wav', null, null, '#1c2efb', null, null, null, null, 1, 1, 1, 0, NOW()
-FROM player
+FROM dual
 WHERE NOT EXISTS (SELECT 1 FROM player WHERE first_name = 'kcapp-bot');
 
 -- +goose Down
